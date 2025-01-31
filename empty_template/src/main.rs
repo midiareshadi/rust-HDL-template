d2609d7a878349b49cc220ac8922c33ff290be8f
Midia Reshadi <midia.reshadi@gmail.com>
2025-01-31 16:16:26 +0000

    Update main.rs

--- empty_template/src/main.rs
+++ empty_template/src/main.rs
@@ -6,6 +6,17 @@ use std::num::Wrapping;
 // Clock frequency
 const clock_freq_HZ : u64 = 100_000_000;
 
+#[derive(LogicBlock, Default)]
+#[cfg(FALSE)]
+{
+   struct Adder8b {
+       pub sig_a: Signal<In, Bits<8>>,
+       pub sig_b: Signal<In, Bits<8>>,
+       pub sig_sum: Signal<Out, Bits<8>>,
+       pub clock: Signal<In, Clock>,
+       my_reg: DFF<Bits<8>>,
+   }
+}
 fn main() {
    empty_function();
 }
