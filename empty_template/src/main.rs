ad6948f0728942dc1ea538060b38baeb5d6881c3
Midia Reshadi <midia.reshadi@gmail.com>
2025-01-31 16:35:09 +0000

    Update main.rs

--- empty_template/src/main.rs
+++ empty_template/src/main.rs
@@ -1,24 +1,28 @@
 // Import necessary modules and traits
-use rust_hdl::prelude::*; 
-use rand::{thread_rng, Rng};
+use rust_hdl::prelude::*;
 use std::num::Wrapping;
 
 // Clock frequency
 const clock_freq_HZ : u64 = 100_000_000;
 
+/*
 #[derive(LogicBlock, Default)]
-#[cfg(FALSE)]
-{
-   struct Name {
-       pub sig_in1: Signal<In, Bits<n_bit>>,       
-       pub sig_out: Signal<Out, Bits<n_bit>>,
-       pub clock: Signal<In, Clock>,
-       my_reg: DFF<Bits<n_bit>>,
+struct Name {
+  pub sig_in1: Signal<In, Bits<n_bit>>,
+  pub sig_out: Signal<Out, Bits<n_bit>>,
+  pub clock: Signal<In, Clock>,
+  my_reg: DFF<Bits<n_bit>>,
+ }
+
+impl Logic for Name {
+ i #[hdl_gen]
+  fn update(&mut self) {
+
    }
 }
+
+*/
+
 fn main() {
-   empty_function();
 }
 
-fn empty_function() {
-}
