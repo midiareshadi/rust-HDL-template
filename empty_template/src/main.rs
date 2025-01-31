d0ec5b3b0f2fc078b6cd5387305481f33cfc3da8
Midia Reshadi <midia.reshadi@gmail.com>
2025-01-31 15:54:48 +0000

    Update main.rs

--- empty_template/src/main.rs
+++ empty_template/src/main.rs
@@ -1,4 +1,11 @@
-// Empty template
+// Import necessary modules and traits
+use rust_hdl::prelude::*; 
+use rand::{thread_rng, Rng};
+use std::num::Wrapping;
+
+// Clock frequency
+const clock_freq_HZ : u64 = 100_000_000;
+
 fn main() {
    empty_function();
 }
